37cbfe20f37d5dc9debeb1391aa3cfb90ce519b4
taqikun <ataqiyudin21@gmail.com>
2022-04-18 09:48:13 +0700

    ..

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from bs4 import BeautifulSoup
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 import requests
 import re
 
 app = FastAPI()
+#app.add_middleware(HTTPSRedirectMiddleware)
 
 url = 'https://harga-emas.org/widget/widget.php?v_widget_type=current_gold_price&v_width=300&v_height=215'
 pages = requests.get(url)
@@ -19,7 +21,7 @@ status = tdstatus[1]
 harga = tdharga[0]
 
 
-@app.get("/harga-emas-terkini")
+@app.get("/api/harga-emas")
 async def root():
     return {"harga_emas": int(harga.replace(".", "")),
             "updated": status,
